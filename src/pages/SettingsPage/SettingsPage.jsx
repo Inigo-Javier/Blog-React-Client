b7a016d0b65f60205bdb4168eff72663cb654682
Iñigo Malluguiza <malluguizamae@gmail.com>
2022-06-27 19:06:10 +0200

    settings file form

--- src/pages/SettingsPage/SettingsPage.jsx
+++ src/pages/SettingsPage/SettingsPage.jsx
@@ -8,24 +8,33 @@ export default function SettingsPage() {
                 <div className='settingsTitle'>
                     <span className='settingsUpdateTitle'>Update Your Account</span>
                     <span className='settingsDeleteTitle'>Delete Account</span>
-                    <form className='settingsForm'>
-                        <label>Profile Picture</label>
-                        <div className='settingsPP'>
-                            <img src='https://cdn.pixabay.com/photo/2016/01/13/20/24/autumn-landscape-1138875_1280.jpg' alt='landscape' />
-                            <label htmlFor='fileInput'>
-                                <i className="settingsPPIcon fa-solid fa-user"></i>
-                            </label>
-                            <input type='file' id='fileInput' slyle={{ display: "none" }}></input>
-                        </div>
-                        <label>Username</label>
-                        <input type="text" placeholder='Iñigo'></input>
-                        <label>Email</label>
-                        <input type="email" placeholder='iñigo@gmail.com'></input>
-                        <label>Password</label>
-                        <input type="password"></input>
-                        <button className='settingsSubmit'>Update</button>
-                    </form>
                 </div>
+                <form className='settingsForm'>
+
+                    <label>Profile Picture</label>
+
+                    <div className='settingsPP'>
+                        <img src='https://cdn.pixabay.com/photo/2016/01/13/20/24/autumn-landscape-1138875_1280.jpg' alt='landscape' />
+
+                        <label htmlFor='fileInput'>
+
+                            <i className="settingsPPIcon fa-solid fa-user"></i>
+
+                        </label>
+                        
+                        <input type='file' id='fileInput' slyle={{ display: "none" }}></input>
+                    </div>
+
+                    <label>Username</label>
+
+                    <input type="text" placeholder='Iñigo'></input>
+                    <label>Email</label>
+                    <input type="email" placeholder='iñigo@gmail.com'></input>
+                    <label>Password</label>
+                    <input type="password"></input>
+
+                    <button className='settingsSubmit'>Update</button>
+                </form>
             </div>
             <Sidebar />
         </div>
