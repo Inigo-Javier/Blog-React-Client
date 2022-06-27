a3c408a3726aca7602dab19b60d4915b6049d30a
Iñigo Malluguiza <malluguizamae@gmail.com>
2022-06-27 11:19:59 +0200

    update SinglePost

--- src/components/SinglePost/SinglePost.jsx
+++ src/components/SinglePost/SinglePost.jsx
@@ -7,7 +7,20 @@ export default function SinglePost() {
                 <img src='https://www.militariazone.com/upload/images/shopprod/25552/a-bridge-too-far-movie-still-featuring-anthony-hopkins_25552_pic1_size4.webp' alt='' className='singlePostImg' />
                 <h1 className='singlePostTitle'>
                     title a bridge too far
+                    <div className='singlePostEdit'>
+                        <i className="singlePostIcon fa-solid fa-pen-to-square"></i>
+                        <i className="singlePostIcon fa-solid fa-trash-can"></i>
+                    </div>
                 </h1>
+                <div className='singlePostInfo'>
+                    <span className='singlePostAutor'>Autor:<b>Safak</b></span>
+                    <span className='singlePostDate'>1 hour ago</span>
+                </div>
+                <p className='singlePostDescription'>
+                    Loremmwdcojkwevnwrvwr vgr   bgt3q4cjnyrjn tyc jheathteh eth etahje hjet het h eth etrh eth et heath etrt he th eH ETH ET HEThetheatah
+
+                    Loremmwdcojkwevnwrvwr vgr   bgt3q4cjnyrjn tyc jheathteh eth etahje hjet het h eth etrh eth et heath etrt he th eH ETH ET HEThetheatahLoremmwdcojkwevnwrvwr vgr   bgt3q4cjnyrjn tyc jheathteh eth etahje hjet het h eth etrh eth et heath etrt he th eH ETH ET HEThetheatahLoremmwdcojkwevnwrvwr vgr   bgt3q4cjnyrjn tyc jheathteh eth etahje hjet het h eth etrh eth et heath etrt he th eH ETH ET HEThetheatahLoremmwdcojkwevnwrvwr vgr   bgt3q4cjnyrjn tyc jheathteh eth etahje hjet het h eth etrh eth et heath etrt he th eH ETH ET HEThetheatahLoremmwdcojkwevnwrvwr vgr   bgt3q4cjnyrjn tyc jheathteh eth etahje hjet het h eth etrh eth et heath etrt he th eH ETH ET HEThetheatah
+                </p>
             </div>
         </div>
     )
