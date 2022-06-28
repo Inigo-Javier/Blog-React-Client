f06c09b07b195942de302d47f3f81c62e271f6d6
Iñigo Malluguiza <malluguizamae@gmail.com>
2022-06-28 10:56:23 +0200

    login page

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,17 @@
 import HomePage from "../pages/HomePage/HomePage";
+import LoginPage from "../pages/LoginPage/LoginPage";
 import SettingsPage from "../pages/SettingsPage/SettingsPage";
 import SinglePage from "../pages/SinglePage/SinglePage";
 import WritePage from "../pages/WritePage/WritePage";
 import TopBar from "./Topbar/TopBar";
 
+
+
 function App() {
   return (
     <>
       <TopBar />
-      <SettingsPage />
+      <LoginPage />
     </>
   );
 }
---
+++ src/pages/LoginPage/LoginPage.jsx
@@ -0,0 +1,17 @@
+import './LoginPage.css'
+
+export default function LoginPage() {
+    return (
+        <div className='login'>
+            <span className="loginTitle">Login</span>
+            <form className='loginForm'>
+                <label>Email</label>
+                <input type="text" placeholder='Enter your email...'></input>
+                <label>Password</label>
+                <input type="password" placeholder='Enter your password...'></input>
+                <button className='loginButton'>Login</button>
+            </form>
+            <button className='loginRegisterButton'>Register</button>
+        </div>
+    )
+}
